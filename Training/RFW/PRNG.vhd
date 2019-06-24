d530bfc7c723b2f5db74c9e4b844ff5ac52d2c06
aabdosobhy <aabdosobhy@gmail.com>
2019-06-24 13:46:36 +0200

    changing PRNG

--- Training/RFW/PRNG.vhd
+++ Training/RFW/PRNG.vhd
@@ -1,7 +1,10 @@
-    library ieee;
-    use ieee.std_logic_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity PRNG is
+    Generic (
+        SEED : std_logic_vector := "11100111"
+    );
     port (
         clk : in std_logic;
         rst : in std_logic;
@@ -12,39 +15,22 @@ entity PRNG is
     
 architecture PRNG7542 of PRNG is
 
-    component lfsr is
-            generic(
-                size : integer := 8
-                );
-            port (
-                clk : in std_logic;
-                rst : in std_logic;
-                enb : in std_logic;
-                LSin : in std_logic;
-                LSout : out std_logic_vector(size -1 downto 0)
-            );         
-    end component;
-
-    signal lfsr_I : std_logic;
-    signal lfsr_O : std_logic_vector(7 downto 0);
-    signal feed_1 : std_logic;
-    signal feed_2 : std_logic;
-    signal feed_3 : std_logic;
+	signal feed1, feed2, feed : std_logic;
+    signal lfsr : std_logic_vector(7 downto 0) := SEED;
     
 begin
-    lfsr_reg : lfsr 
-        generic map (
-            SIZE => 8)
-        port map (
-            clk => clk,
-            enb => enb,
-            rst => rst,
-            LSin => lfsr_I,
-            LSout => lfsr_O
-        );
     
-    feed_1 <= lfsr_O(0) xnor lfsr_O(2);
-    feed_2 <= feed_1 xnor lfsr_O(3);
-    feed_3 <= feed_2 xnor lfsr_O(5);
-    lfsr_I <= feed_3;
+    process(clk, rst)
+    begin
+        if rst = '1' then
+            lfsr <= SEED;
+        elsif rst = '0' and rising_edge(clk) and enb = '1' then
+            lfsr <= feed & lfsr(7 downto 1);
+        end if;
+    end process;
+	feed1 <= lfsr(0) xor lfsr(2);
+	feed2 <= feed1 xor lfsr(3);
+    feed <= feed2 xor lfsr(5);
+    PRNG_O <= lfsr(0);
+
 end PRNG7542;
