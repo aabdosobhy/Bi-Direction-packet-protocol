b791feb7b77dc9b09965fd861e0af47e68b6336c
aabdosobhy <aabdosobhy@gmail.com>
2019-08-03 04:14:44 +0200

    fixed tcl file.

--- Training/ZYNQ/top.tcl
+++ Training/ZYNQ/top.tcl
@@ -1,100 +1,32 @@
-#---------------------------------------------------------------------------------
-#- Company:        apertus° Association
-#- Engineer:       Abd-ElRhman Sobhy
-#- 
-#- Create Date:    28/6/2019 
-#- Design Name:    Link training between ZYNQ and MAchXO2 fpga (ZYNQ side)
-#- Module Name:    Tcl commands 
-#- Target Devices: Zynq-7000  xc7z020clg400-1
-#- Tool versions:  xilinx vivado
-#- Description:    Tcl command files
-#-
-#- Dependencies: 
-#-
-#- Revision: 
-#- Revision 0.01 - File Created
-#- Additional Comments: 
-#-
-#---------------------------------------------------------------------------------
-#- This program is free software: you can redistribute it and/or
-#- modify it under the terms of the GNU General Public License
-#- as published by the Free Software Foundation, either version
-#- 3 of the License, or (at your option) any later version.
-#----------------------------------------------------------------------------------
+set pwd [pwd]
 
+#cd $work_directory
 
+set OS [lindex $tcl_platform(os) 0]
+if { $OS == "Windows" } {
+    exec cmd /c mkdir build.vivado
+} else {
+    exec mkdir build.vivado
+}
+cd build.vivado
 
+create_project Train_ZYNQ [pwd]
 
+read_vhdl -vhdl2008 ../register.vhd
+read_vhdl -vhdl2008 ../Shift_reg.vhd
+read_vhdl -vhdl2008 ../8b10_enc.vhd
+read_vhdl -vhdl2008 ../PRNG.vhd
+read_vhdl -vhdl2008 ../serializer.vhd
+read_vhdl -vhdl2008 ../train.vhd
+read_vhdl -vhdl2008 ../top.vhd
 
-# Software version: Xilinx Vivado 2017.4
-# Product Family: Zynq-7000 
-# Target Device: xc7z020clg400-1
-# Language: VHDL
+read_xdc ../top.xdc
 
-# reading all files 
-read_vhdl -vhdl2008 register.vhd
-read_vhdl -vhdl2008 Shift_reg.vhd
-read_vhdl -vhdl2008 8b10_enc.vhd
-read_vhdl -vhdl2008 PRNG.vhd
-read_vhdl -vhdl2008 serializer.vhd
-read_vhdl -vhdl2008 train.vhd
+set_property part xc7z020clg484-1 [current_project]
 
-read_xdc top.xdc
-
-update_compile_order -fileset sources_1
-update_compile_order -fileset sources_1
-launch_simulation
-
-set_property PART xc7z020clg400-1 [current_project]
 set_property TARGET_LANGUAGE VHDL [current_project]
 
-add_force {/train/clk} -radix hex {1 0ns} {0 50000ps} -repeat_every 100000ps
-add_force {/train/rst} -radix hex {1 0ns}
-run 100 ns
-add_force {/train/rst} -radix hex {0 0ns}
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
-run 100 ns
 
+launch_runs synth_1
 
-
-
-
-
+launch_runs impl_1
--- Training/ZYNQ/top.xdc
+++ Training/ZYNQ/top.xdc
@@ -1,1 +1,11 @@
 create_clock -period 10.000 -name sysClk -waveform {0.000 5.000} [get_ports -filter { NAME =~  "*clk*" && DIRECTION == "IN" }]
+
+# RFW 
+set_property PACKAGE_PIN T9 [get_ports -filter { NAME =~  "*lvds_p*" && DIRECTION == "IN" }]
+set_property PACKAGE_PIN U10 [get_ports -filter { NAME =~  "*lvds_n*" && DIRECTION == "IN" }]
+
+# RFE
+#set_property PACKAGE_PIN T9 [get_ports -filter { NAME =~  "*lvds_p*" && DIRECTION == "IN" }]
+#set_property PACKAGE_PIN U10 [get_ports -filter { NAME =~  "*lvds_n*" && DIRECTION == "IN" }]
+#set_property PACKAGE_PIN T9 [get_ports -filter { NAME =~  "*lvds_p*" && DIRECTION == "IN" }]
+#set_property PACKAGE_PIN U10 [get_ports -filter { NAME =~  "*lvds_n*" && DIRECTION == "IN" }]
